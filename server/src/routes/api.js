ba11ce7194b1ab4740e228b605500b1fe0a491b7
Khushi Garg <khushi999garg@gmail.com>
2023-09-03 00:55:19 +0530

    Login

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,9 +1,12 @@
 import express from"express";
 import Register from "../controllers/register.controller.js";
 import { RegisterSchema } from "../validationSchema/registerSchema.js";
+import Login from "../controllers/login.controller.js";
+import { LoginSchema } from "../validationSchema/loginSchema.js";
 
 const route = express.Router();
 
 route.post('/register', RegisterSchema, Register);
+route.post('/login', LoginSchema, Login);
 
 export default route;
